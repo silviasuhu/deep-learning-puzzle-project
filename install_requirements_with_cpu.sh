a79a86aef535ad1677f2b36175d336f7e7b98ce8
Silvia <silvia.suhu@gmail.com>
2026-02-07 09:42:40 +0100

    Improve guidelines to install python dependencies

---
+++ install_requirements_with_cpu.sh
@@ -0,0 +1,9 @@
+#bin/bash
+
+. .venv/bin/activate
+
+pip install torch==2.6.0 torchvision==0.21.0 --index-url https://download.pytorch.org/whl/cpu
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.6.0+cpu.html
+pip install torch-geometric
+
+pip install -r requirements.txt
